47c74a16ee5388ef1160ceeaa57b4b74d3b03cc0
Ishaan Bhargava <ishaan.bhargava22@imperial.ac.uk>
2025-03-05 15:13:02 +0000

    particle class now uses blas implementation

--- particle.h
+++ particle.h
@@ -6,17 +6,26 @@
 #include <tuple>
 using namespace std;
 
+#include <cblas.h>
+
 class Particle {
     public:
 
-        double* position = new double[3];      // Position
-        double vx, vy, vz;   // Velocity
+        double* r = new double[3];      // Position
+        double* v = new double[3];      // Velocity
+        double* F = new double[3];      // Force at time t
         int type;            // Particle type = 0 or 1
         double mass;         // Mass (1 for type = 0, 10 for type = 1)
         
         // Constructor: create particles with its position, velocity and type(mass)
         Particle(double x_i, double y_i, double z_i, double vx_i, double vy_i, double vz_i, int type_i) 
-        : x(x_i), y(y_i), z(z_i), vx(vx_i), vy(vy_i), vz(vz_i), type(type_i) {  
+        : type(type_i) {  
+            r[0] = x_i;
+            r[1] = y_i;
+            r[2] = z_i;
+            v[0] = vx_i;
+            v[1] = vy_i;
+            v[2] = vz_i;
             if (type == 0) {
                 mass = 1.0;
             } else {
@@ -29,35 +38,31 @@ class Particle {
             // nothing to do
         }
 
+        void applyBC(double* pos, double* vel, double L) {
+            if (*pos < 0) {
+                *pos = -*pos;  // Reflect inside the boundary
+                *vel = abs(*vel);
+            }
+            if (*pos > L) {
+                *pos = 2 * L - *pos;
+                *vel = -abs(*vel);
+            }
+        }
+
         // Updates the particles position and velocity:
-        // r(i+1) = r(i) + dt * v(i)
-        // v(i+1) = v(i) = dt * F(i)/mass
-        // check boundary conditions after
         void updatePosition(double dt, double Lx, double Ly, double Lz, double Fx, double Fy, double Fz) {
-            x += dt * vx;    // update x
-            y += dt * vy;    // update y
-            z += dt * vz;    // update z
-
-            vx += dt * Fx/mass;  // update vx
-            vy += dt * Fy/mass;  // update vy
-            vz += dt * Fz/mass;  // update vz
+            cblas_daxpy(3, dt, v, 1, r, 1);    // update position r = r + dt*v
+            cblas_daxpy(3, dt/mass, F, 1, v, 1);  // update velocity v = v + dt/mass * F
             
-            //check x boundary condition
-            if (x < 0) {x = -x; vx = abs(vx);} 
-            if (x > Lx) {x = 2*Lx - x; vx = -abs(vx);}
-
-            //check y boundary condition
-            if (y < 0) {y = -y; vy = abs(vy);} 
-            if (y > Ly) {y = 2*Ly - y; vy = -abs(vy);}
-
-            //check z boundary condition
-            if (z < 0) {z = -z; vz = abs(vz);} 
-            if (z > Lz) {z = 2*Lz - z; vz = -abs(vz);}
+            // Apply the boundary conditions
+            applyBC(&r[0], &v[0], Lx);
+            applyBC(&r[1], &v[1], Lx);
+            applyBC(&r[2], &v[2], Lx);
         }
         
         // calcaultes kinetic energy of particle
         double particleKE() {
-            return 0.5 * mass * (vx*vx + vy*vy + vz*vz);
+            return 0.5 * mass * cblas_dnrm2(3, v, 1);
         }
 };
 
