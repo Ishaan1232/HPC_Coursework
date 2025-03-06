d93f3834794a4cf8cfa2b889d1d895b620b2f0c7
Ishaan Bhargava <ishaan.bhargava22@imperial.ac.uk>
2025-03-06 19:49:23 +0000

    separated particle.h into particle.h and particle.cpp, also changed dynamic memory to arrays since there were sometimes issues with memory allocation

--- particle.h
+++ particle.h
@@ -13,6 +13,16 @@ class Particle {
         vector<double> F;       // Force at time t
         int type;               // Particle type, either 0 or 1
         double mass;            // Mass (1 for type 0 or 10 for type 1)
+
+    public: 
+        Particle(double x_i, double y_i, double z_i, double vx_i, double vy_i, double vz_i, int type_i);    // Constructor
+        Particle(const Particle& p);    // Copy Constructor
+        ~Particle() = default;          // Destructor
+
+        void applyBC(double& pos, double& vel, double L);
+        void updatePosition(double dt, double Lx, double Ly, double Lz, double temp, double E);
+        double particleKE(); 
+        
 };
 
 #endif; // PARTICLE_H
