4fbded429f488cd012b55bad61e2e1f05449a57a
Ishaan Bhargava <ishaan.bhargava22@imperial.ac.uk>
2025-03-18 18:56:42 +0000

    BEgginign CUDA implementation

--- Makefile
+++ Makefile
@@ -1,16 +1,20 @@
 CXX = g++
 CXXFLAGS = -std=c++11 -Wall -O2 -ftree-vectorize
-CXXFLAGSPAR = $(CXXFLAGS) -fopenmp 
+
 HDRS = parse_cl.h box.h particle.h
+HDRS_CUDA = parse_cl.h box.cuh particle.h
 
 OBJS = dynamics.o parse_cl.o box.o particle.o
-PAR_OBJS = dynamicsPar.o parse_clPar.o boxPar.o particlePar.o 
+PAR_OBJS = dynamics.o parse_cl.o boxPar.o particlePar.o 
+CUDA_OBJS = dynamics.o parse_cl.o boxCuda.o particle.o
 
 TEST_OBJS = test.o box.o particle.o
 TEST_PAR_OBJS = testPar.o boxPar.o particlePar.o
+TEST_CUDA_OBJS = testCuda.o boxCuda.o particle.o
 
 LDLIBS = -lblas -lboost_program_options -lboost_unit_test_framework
-BUILDS = md mdpar unittests unittestsPar
+BUILDS = md mdpar mdcuda unittests unittestsPar unittestsCuda
+FILES = html latex Doxyfile
 
 default: md
 
@@ -20,23 +24,9 @@ default: md
 md : $(OBJS)
 	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDLIBS)
 
-test.o: test.cpp
-	$(CXX) $(CXXFLAGS) -o $@ -c $<
-
 unittests: $(TEST_OBJS) 
 	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDLIBS)
 
-%Par.o : %Par.cpp $(HDRS)
-	$(CXX) $(CXXFLAGSPAR) -o $@ -c $<
-
-mdpar : $(PAR_OBJS)
-	$(CXX) $(CXXFLAGSPAR) -o $@ $^ $(LDLIBS)
-
-testPar.o: testPar.cpp
-	$(CXX) $(CXXFLAGSPAR) -o $@ -c $<
-
-unittestsPar: $(TEST_PAR_OBJS) 
-	$(CXX) $(CXXFLAGSPAR) -o $@ $^ $(LDLIBS)
 
 doc: Doxyfile
 	doxygen $<
@@ -44,7 +34,25 @@ doc: Doxyfile
 Doxyfile:
 	doxygen -g $@
 
+
+mdpar : CXX += -fopenmp
+mdpar : $(PAR_OBJS)
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDLIBS)
+
+unittestsPar : CXX += -fopenmp
+unittestsPar: $(TEST_PAR_OBJS) 
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDLIBS)
+
+
+%.o : %.cu $(HDRS_CUDA)
+	$(CXX) $(CXXFLAGS) -o $@ -c $<
+
+mdcuda : CXX = nvcc
+mdcuda : $(CUDA_OBJS)
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDLIBS)
+
+
 .PHONY : clean
 
 clean : 
-	-rm -f *.o $(BUILDS) -rf html latex Doxyfile
+	-rm -f *.o $(BUILDS) -rf $(FILES)
---
+++ boxCuda.cuh
@@ -0,0 +1,55 @@
+/**
+ * @file box.h
+ * @brief Defines the Box class
+ * 
+ * This header declares the Box class, which manages particles and their interactions
+ * within a 3D simulation environment.
+ */
+
+#ifndef BOX_H
+#define BOX_H// header guard to prevent being included in multiple files
+
+#include <cmath> 
+#include <vector>
+#include <fstream> 
+#include <iomanip>
+#include <string>
+#include <array>
+#include "particle.h"
+/**
+ * @class Box
+ * @brief Represents the simulation box 
+ */
+class Box {
+    private:
+        const double Lx, Ly, Lz;   ///< Box dimensions in the x, y and z directions
+        vector<Particle> particles;  ///< Vector to hold particles
+        int N;                  ///< Number of particles
+
+    public:
+        /// @brief Default constructor
+        Box();
+        /// @brief Construct the test box with the given dimensions
+        Box(const double x_length, const double y_length, const double z_length);  
+        /// @brief Default destructor
+        ~Box() = default;
+        /// @brief Adds a particle to the box and ensures there is a minimum separation.
+        bool addParticle(Particle& p); 
+        /// @brief Calculate the force on a particle exerted by all other particles
+        void calculateF_i(Particle& p_i, int i);   
+        /// @brief Return the kinetic energy of the system
+        double systemKE();
+        /// @brief Run the simulation once all particles are added successfully
+        void runSimulation(double dt, double T, double temp, bool ic_random, string ic); 
+
+        /**
+         * @brief Getter for particles vector 
+         * 
+         * Used for unittests
+         * 
+         * @return Returns a pointer to the particles vector.
+         * */
+        const vector<Particle>& get_particles() const { return particles; }  // Read-only version
+};
+
+#endif // BOX_H
